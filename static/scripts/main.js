e44be7fd5e7cb5a42fdb02d205ed16f221ff3679
Samuel Gratzl <samuel_gratzl@gmx.at>
2014-07-07 09:41:33 +0200

    more data metadata

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -5,9 +5,25 @@ $(function () {
     var $data = d3.select('#datasets').selectAll('li').data(data);
     $data.enter()
       .append('li')
-      .text(function (d) {
-        return d.name;
+      .each(function (d) {
+        d3.select(this).append('a')
+          .text(d.name)
+          .attr('href', '#')
+          .on('click', function (d) {
+            d3.json('api/dataset/'  + d.id, function (file) {
+              console.log(file);
+            });
+          });
+        var d2 = d3.select(this).append('table').selectAll('tr').data(d3.entries(d));
+        d2.enter()
+          .append('tr').each(function (d) {
+            var row = d3.select(this);
+            row.append('td').text(d.key);
+            row.append('td').text(JSON.stringify(d.value));
+          });
+        d2.exit().remove();
       });
+
     $data.exit().remove();
   });
 });
