c1e1f085b5b7f62b5c5cfedb38b84c55f77ca5ad
Samuel Gratzl <samuel_gratzl@gmx.at>
2014-07-16 09:22:11 +0200

    very primitive heatmap for matrices

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -6,9 +6,12 @@ $(function () {
     return arg;
   }
 
-  var loadFile = function (desc, file) {
+  function loadTable(desc, file) {
+    var $base = d3.select("#filetable").style("display", null);
+    d3.select("#fileheatmap").style("display", "none");
+
     var headers = file.cols;
-    var $base = d3.select("#file");
+
     $base.select("caption").text(desc.name);
     var $headers = $base.select("thead tr").selectAll("th").data(headers);
     $headers.enter()
@@ -25,7 +28,34 @@ $(function () {
       $row.exit().remove();
     });
     $rows.exit().remove();
-  };
+  }
+
+  function loadHeatMap(desc, file) {
+    var $svg = d3.select("#fileheatmap").style("display", null);
+    d3.select("#filetable").style("display", "none");
+    var colScale = d3.scale.linear().domain([0, file.cols.length]).range([0, 100]);
+    var rowScale = d3.scale.linear().domain([0, file.rows.length]).range([0, 100]);
+    var c = d3.scale.linear().domain([0, 1]).range(["black", "white"]);
+    var $rows = $svg.selectAll("g").data(file.data);
+    $rows.enter().append("g");
+    $rows.each(function (row, i) {
+      var $cols = d3.select(this).selectAll("rect").data(row);
+      $cols.enter().append("rect");
+      $cols.attr({
+        fill: function (d) {
+          return c(d);
+        },
+        x: function (d, j) {
+          return colScale(j) + "%";
+        },
+        y: rowScale(i) + "%",
+        width: colScale(1) + "%",
+        height: rowScale(1) + "%"
+      });
+      $cols.exit().remove();
+    });
+    $rows.exit().remove();
+  }
 
   d3.json('api/dataset', function (data) {
     var $data = d3.select('#datasets').selectAll('li').data(data);
@@ -37,7 +67,11 @@ $(function () {
           .attr('href', '#')
           .on('click', function (d) {
             d3.json('api/dataset/'  + d.id, function (file) {
-              loadFile(d, file);
+              if (d.type === 'matrix') {
+                loadHeatMap(d, file);
+              } else {
+                loadTable(d, file);
+              }
             });
           });
         var d2 = d3.select(this).append('table').selectAll('tr').data(d3.entries(d));
