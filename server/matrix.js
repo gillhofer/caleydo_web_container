d7baeeeedd3a2db35e3fefe1bac078f9d4fb9bca
Samuel Gratzl <samuel_gratzl@gmx.at>
2014-07-16 09:58:07 +0200

    more matrix utility functions

--- server/matrix.js
+++ server/matrix.js
@@ -22,6 +22,12 @@ Matrix.prototype = {
   at: function (row, col) {
     return this.data[row][col];
   },
+  row: function(i) {
+    return this.data[i];
+  },
+  col: function(j) {
+    return this.data.map(function(row) { return row[j]; });
+  },
   rslice: function (start, end) {
     return new Matrix(this.cols, this.rows.slice(start, end), this.data.slice(start, end));
   },
@@ -29,6 +35,22 @@ Matrix.prototype = {
     return new Matrix(this.cols.slice(start, end), this.rows, this.data.map(function (row) {
       return row.slice(start, end);
     }));
+  },
+  slice: function (rstart, rend, cstart, cend) {
+    return new Matrix(this.cols.slice(cstart, cend), this.rows.slice(rstart, rend), this.data.slice(rstart, rend).map(function (row) {
+      return row.slice(cstart, cend);
+    }));
+  },
+  map : function (f) {
+    var d = this.data.map(function(row) {
+      return row.map(f);
+    });
+    return new Matrix(this.cols, this.rows, d);
+  },
+  toString : function(colSep, rowSep) {
+    colSep = colSep || ",";
+    rowSep = rowSep || "\n";
+    return this.data.map(function(row) { return row.join(colSep)}).join(rowSep);
   }
 };
 
