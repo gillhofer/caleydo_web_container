93acc73a571c2c1b9456aa49c76d0129da330c3e
Samuel Gratzl <samuel_gratzl@gmx.at>
2014-07-17 16:47:42 +0200

    start with idtypes

--- app/scripts/caleydo-idtypes.js
+++ app/scripts/caleydo-idtypes.js
@@ -2,15 +2,30 @@
  * Created by Samuel Gratzl on 17.07.2014.
  */
 /*global define */
-define(['caleydo'], function (C) {
+define(['caleydo','caleydo-events'], function (C, events) {
   'use strict';
-  var cache = {};
+  var cache = undefined;
+
+  function IDType() {
+    events.EventHandler.call(this);
+  }
+  IDType.prototype = new events.EventHandler;
+
+  function load() {
+    if(cache) {
+      return C.resolved(cache);
+    }
+    return C.getJSON('data/idtypes.json').then(function(c) {
+      cache = c;
+      return c;
+    });
+  }
   return {
     resolve : function(name) {
       return name; //FIXME cache and resolve
     },
     list : function() {
-      return cache;
+      return load();
     },
     register: function(name, idtype) {
       if (cache.hasOwnProperty(name)) {
@@ -18,6 +33,7 @@ define(['caleydo'], function (C) {
       }
       cache[name] = idtype;
       return idtype;
-    }
-  }
+    },
+    IDType : IDType
+  };
 });
