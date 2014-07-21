c54db40d6882fec9f7adfc12a15a312bb001b330
Samuel Gratzl <samuel_gratzl@gmx.at>
2014-07-21 15:40:14 +0200

    continue with creating range tests

--- test/spec/test-caleydo-range.js
+++ test/spec/test-caleydo-range.js
@@ -18,6 +18,42 @@ define(['../scripts/caleydo-range','../scripts/caleydo-iterator'], function (ran
       expect(range.from(10).toString()).to.be.equal('10:-1');
       expect(range.from(10).step(2).toString()).to.be.equal('10:-1:2');
       expect(range.list([1,2,3]).toString()).to.be.equal('(1,2,3)');
-    })
+
+      expect(range.from(10,3).toString()).to.be.equal('10:-1,3:-1');
+      expect(range.from(10,undefined,3).toString()).to.be.equal('10:-1,,3:-1');
+    });
+
+    it('parse', function() {
+      var r;
+      function check(dim, from,to,step) {
+        expect(dim).respondTo('from').respondTo('to').respondTo('step');
+        expect(dim).property('isAll',false);
+        expect(dim).property('isList',false);
+        expect(dim.from()).is.equal(from);
+        expect(dim.to()).is.equal(to);
+        expect(dim.step()).is.equal(step);
+      }
+      expect(range.parse('')).have.property('isAll',true);
+      r = range.parse('10:-1');
+      expect(r.dims).is.length(1);
+      check(r.dim(0), 10, -1, 1);
+
+      r = range.parse('10:-1:2');
+      check(r.dim(0), 10, -1, 2);
+      r = range.parse('(1,2,3)');
+      expect(r.dims).is.length(1);
+      expect(r.dim(0).isList).is.true;
+
+      r = range.parse('10:-1,3:-1');
+      expect(r.dims).is.length(2);
+      check(r.dim(0), 10, -1, 1);
+      check(r.dim(1), 3, -1, 1);
+
+      r = range.parse('10:-1,,3:-1');
+      expect(r.dims).is.length(3);
+      check(r.dim(0), 10, -1, 1);
+      expect(r.dim(1).isAll).is.true;
+      check(r.dim(0), 3, -1, 1);
+    });
   });
 });
