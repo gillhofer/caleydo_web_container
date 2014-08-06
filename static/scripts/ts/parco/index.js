e005b4d5b93ad7c8464047b6e6cecf503a71b1f7
Marc Streit <marc.streit@jku.at>
2014-08-06 15:43:10 -0400

    set data in parcoords added simple scatterplot where user can specify the data that will be mapped onto each axis

--- static/scripts/ts/parco/index.js
+++ static/scripts/ts/parco/index.js
@@ -18,19 +18,15 @@ define(['exports', 'd3', 'd3.parcoords', '../caleydo'], function (exports, d3, d
         style: 'width:360px;height:150px'
       });
 
-      var data = [
-        [0,-0,0,0,0,3 ],
-        [1,-1,1,2,1,6 ],
-        [2,-2,4,4,0.5,2],
-        [3,-3,9,6,0.33,4],
-        [4,-4,16,8,0.25,9]
-      ];
+      this.data.data().then(function (arr) {
 
-      var pc = d3_parcoords()($svg.node())
-        .data(data)
-        .render()
-        .ticks(3)
-        .createAxes();
+        var pc = d3_parcoords()($svg.node())
+          .data(arr)
+          .render()
+          .ticks(3)
+          .createAxes();
+
+      })
     };
     return ParCo;
   })();
--- static/scripts/ts/scatterplot/index.js
+++ static/scripts/ts/scatterplot/index.js
@@ -1,7 +1,7 @@
 /**
  * Created by Marc Streit on 06.08.2014.
  */
-define(['exports', 'd3','../caleydo'], function (exports, d3, C) {
+define(['exports', 'd3', '../caleydo'], function (exports, d3, C) {
   var ScatterPlot = (function () {
     function ScatterPlot(data, parent) {
       this.data = data;
@@ -11,29 +11,108 @@ define(['exports', 'd3','../caleydo'], function (exports, d3, C) {
 
     ScatterPlot.prototype.build = function ($parent) {
       var dims = this.data.dim;
-      var width = dims[1], height = dims[0];
-      var $svg = $parent.append('div').append("svg").attr({
+      var width = 200, height = 200;
+      var div = $parent.append('div');
+
+      var xcol = 0;
+      var ycol = 1;
+
+      var svg = div.append("svg").attr({
         'class': 'scatterplot',
         id: 'example',
         style: 'width:360px;height:150px'
       });
 
-var test = $svg.selectAll(".test").data([1,2,3]);
-test.exit().remove();
+      that = this;
 
-// --- adding Element to class test
-var testEnter = test.enter().append("circle").attr({
-    "class":"test"
-})
+      // bind data to chart
+      this.data.data().then(function (arr) {
 
-// --- changing nodes for test
-test.attr({
-    cx: 5,
-    cy: function (d,i) {
-      return i*10+5+d*2;
-    },
-    r:5
-})
+        // create scales
+        var x = d3.scale.linear().domain([0, d3.max(arr.map(function (d) {
+            return d[xcol]
+          }))]).range([0, width]),
+          y = d3.scale.linear().domain([0, d3.max(arr.map(function (d) {
+            return d[ycol]
+          }))]).range([height, 0]);
+
+        svg.selectAll('circle')
+          .data(arr)
+          .enter().append('circle')
+          .attr("fill", "black")
+          .attr("cx", function (d) {
+            return x(d[xcol]);
+          })
+          .attr("cy", function (d) {
+            return y(d[ycol]);
+          })
+          .attr("r", 2);
+        //.on("mouseover", function(d) {
+        //  d3.select('#hover-food').text(d.name.substr(0,50));
+        //});
+      });
+
+      div.append("select").attr("id", "xaxis-selection");
+      div.append("select").attr("id", "yaxis-selection");
+
+      this.data.cols().then(function (cols) {
+        d3.select("#xaxis-selection").selectAll("option").data(cols).enter().append("option").attr("value", function (d, i) {
+          return i;
+        })
+          .text(function (d) {
+            return d;
+          })
+          .each(function (d, i) {
+            if (i == xcol) d3.select(this).attr("selected", "yes");
+          });
+
+        d3.select("#xaxis-selection")
+          .on("change", function() {
+            xcol = this.selectedIndex;
+            update()
+          });
+
+        d3.select("#yaxis-selection").selectAll("option").data(cols).enter().append("option").attr("value", function (d, i) {
+          return i;
+        })
+          .text(function (d) {
+            return d;
+          })
+          .each(function (d, i) {
+            if (i == ycol) d3.select(this).attr("selected", "yes");
+          });
+
+        d3.select("#yaxis-selection")
+          .on("change", function() {
+            ycol = this.selectedIndex;
+            update()
+          });
+      })
+
+      function update() {
+
+        that.data.data().then(function (arr) {
+
+          // create scales
+          var x = d3.scale.linear().domain([0, d3.max(arr.map(function (d) {
+              return d[xcol]
+            }))]).range([0, width]),
+            y = d3.scale.linear().domain([0, d3.max(arr.map(function (d) {
+              return d[ycol]
+            }))]).range([height, 0]);
+
+          svg.selectAll('circle')
+            .transition()
+            .ease('linear')
+            .duration(1000)
+            .attr("cx", function (d) {
+              return x(d[xcol]);
+            })
+            .attr("cy", function (d) {
+              return y(d[ycol]);
+            })
+        })
+      };
     };
     return ScatterPlot;
   })();
