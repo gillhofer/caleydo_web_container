1fbe7aa75324d3fea749f42aeeb07607121d5e0f
Samuel Gratzl <samuel_gratzl@gmx.at>
2014-10-08 13:20:10 +0200

    highlight selection in parco

--- static/scripts/parco/index.js
+++ static/scripts/parco/index.js
@@ -12,16 +12,50 @@ define(['exports', 'd3', 'd3.parcoords', '../caleydo'], function (exports, d3, d
     var $base = $parent.append('div').attr({
       'class': 'parcoords',
       style: 'width:360px;height:150px'
+    }), data = this.data;
+
+    var pc = d3_parcoords()($base.node());
+    /*, types = {};
+    pc.dimensions(this.data.cols().map(function (col) {
+      var val = col.desc.value, type;
+      switch (val.type) {
+      case 'real':
+      case 'int':
+        type = 'number';
+        pc.scale(col.name, val.range);
+        break;
+      case 'categorical':
+        type = 'string';
+        pc.scale(col.name, val.categories);
+        break;
+      default:
+        type = 'string';
+        break;
+      }
+      types[col.name] = type;
+      return col.name;
+    }));
+    pc.types(types);*/
+
+
+    this.data.objects().then(function (arr) {
+      pc.data(arr);
+      pc.render();
+      pc.ticks(3)
+        .createAxes();
     });
 
-    this.data.data().then(function (arr) {
-
-      var pc = d3_parcoords()($base.node())
-        .data(arr)
-        .render()
-        .ticks(3)
-        .createAxes();
-
+    var l = function (event, type, selected) {
+      var slice = selected.filter(pc.data(), []);
+      //highlight selection
+      pc.highlight(slice);
+    };
+    data.on('select', l);
+    C.onDOMNodeRemoved($base.node(), function () {
+      data.off('select', l);
+    });
+    data.selections().then(function (selected) {
+      l(null, 'selected', selected);
     });
 
     return $base.node();
