b107c53fdf1385b59d587e5ae20e16a5f2e1a5ac
Christian Partl <partl@icg.tugraz.at>
2015-02-05 17:11:14 +0100

    simple static rendering of paths

--- plugins/caleydo-vis-pathway/app.js
+++ plugins/caleydo-vis-pathway/app.js
@@ -6,38 +6,63 @@ require(['jquery', 'd3', '../caleydo/main', '../caleydo/data', '../caleydo/plugi
 
   $(document).ready(function () {
 
-  //  $.ajax({
-  //
-  //    // The 'type' property sets the HTTP method.
-  //    // A value of 'PUT' or 'DELETE' will trigger a preflight request.
-  //    //type: 'POST',
-  //    type: 'GET',
-  //
-  //    // The URL to make the request to.
-  //    url: 'http://localhost:7474/db/data/',
-  //    //url: 'http://localhost:7474/db/data/ext/FindShortestPath/graphdb/get_shortest_path',
-  //
-  //    accepts: 'application/json',
-  //
-  //    success: function (response) {
-  //      d3.select("body").append("p").text(response);
-  //    },
-  //
-  //    error: function (response) {
-  //      var x = 2
-  //    }
-  //  });
+    var w = 500;
+    var h = 500;
+
+    var svg = d3.select("body").append("svg")
+    svg.attr("width", w)
+      .attr("height", h);
+    svg.append("marker")
+      .attr("id", "arrowRight")
+      .attr("viewBox", "0 0 10 10")
+      .attr("refX", "0")
+      .attr("refY", "5")
+      .attr("markerUnits", "strokeWidth")
+      .attr("markerWidth", "4")
+      .attr("markerHeight", "3")
+      .attr("orient", "auto")
+      .append("path")
+      .attr("d", "M 0 0 L 10 5 L 0 10 z");
+    //<marker id="triangle"
+    //viewBox="0 0 10 10" refX="0" refY="5"
+    //markerUnits="strokeWidth"
+    //markerWidth="4" markerHeight="3"
+    //orient="auto">
+    //<path d="M 0 0 L 10 5 L 0 10 z" />
+    //</marker>
+
+    //  $.ajax({
+    //
+    //    // The 'type' property sets the HTTP method.
+    //    // A value of 'PUT' or 'DELETE' will trigger a preflight request.
+    //    //type: 'POST',
+    //    type: 'GET',
+    //
+    //    // The URL to make the request to.
+    //    url: 'http://localhost:7474/db/data/',
+    //    //url: 'http://localhost:7474/db/data/ext/FindShortestPath/graphdb/get_shortest_path',
+    //
+    //    accepts: 'application/json',
+    //
+    //    success: function (response) {
+    //      d3.select("body").append("p").text(response);
+    //    },
+    //
+    //    error: function (response) {
+    //      var x = 2
+    //    }
+    //  });
 
     //$.ajax({
     //
     //  // The 'type' property sets the HTTP method.
     //  // A value of 'PUT' or 'DELETE' will trigger a preflight request.
-    //  type: 'POST',
-    //  //type: 'GET',
+    //  //type: 'POST',
+    //  type: 'GET',
     //
     //  // The URL to make the request to.
-    //  url: 'http://localhost:7474/db/data/ext/KShortestPaths/graphdb/kShortestPaths',
-    //  //url: 'http://localhost:7474/db/data/ext/FindShortestPath/graphdb/get_shortest_path',
+    //  //url: 'http://localhost:7474/db/data/ext/KShortestPaths/graphdb/kShortestPaths',
+    //  url: '/api/pathway/path',
     //
     //  accepts: 'application/json',
     //
@@ -49,11 +74,11 @@ require(['jquery', 'd3', '../caleydo/main', '../caleydo/data', '../caleydo/plugi
     //  // you will trigger a preflight request.
     //  contentType: 'application/json',
     //
-    //  data: '{"source":"http://localhost/:7474/db/data/node/15991", ' +
-    //  '"target":"http://localhost/:7474/db/data/node/1713", ' +
-    //  '"k":"3", ' +
-    //  '"costFunction":"function getCost(properties) {return 1.0}", ' +
-    //  '"ignoreDirection":"false"}',
+    //  //data: '{"source":"http://localhost/:7474/db/data/node/15991", ' +
+    //  //'"target":"http://localhost/:7474/db/data/node/1713", ' +
+    //  //'"k":"3", ' +
+    //  //'"costFunction":"function getCost(properties) {return 1.0}", ' +
+    //  //'"ignoreDirection":"false"}',
     //  //data: '{"node_from": "/node/116", "node_to": "/node/92", "relationship_types": ["PATHWAY_EDGE"], "relationship_costs": [1.0], "only_one_route": true, "soft_timeout": 5000, "max_cost": 1000000.0 }',
     //
     //  //xhrFields: {
@@ -74,8 +99,8 @@ require(['jquery', 'd3', '../caleydo/main', '../caleydo/data', '../caleydo/plugi
     //  success: function (response) {
     //    d3.select("body").append("p").text(response);
     //    //d3.select("body").append("img").attr("src", "http://rest.kegg.jp/get/hsa00052/image");
-    //    var paths = JSON.parse(response)
-    //
+    //    var paths = JSON.parse(response);
+    //    //var p = JSON.parse(paths);
     //  },
     //
     //  error: function (response) {
@@ -119,10 +144,102 @@ require(['jquery', 'd3', '../caleydo/main', '../caleydo/data', '../caleydo/plugi
 
 
     $.get("/api/pathway/path", function (resp) {
-      $('<h1>'+resp+'</h1>').appendTo('body');
+
+      var paths = JSON.parse(resp);
+
+      renderPaths(svg, paths);
+      //$('<h1>' + resp + '</h1>').appendTo('body');
     });
 
   });
+
+  function renderPaths(svg, paths) {
+
+    var posX = 20;
+    var posY = 20;
+
+    paths.forEach(function (path) {
+      var p = svg.append("g");
+      p.attr("class", "path");
+      renderPath(p, path, posX, posY);
+      posY += 50;
+    });
+  }
+
+  function renderPath(p, path, posX, posY) {
+    var nodeWidth = 50;
+    var nodeHeight = 20;
+    var vSpacing = 10;
+    var edgeSize = 50;
+    var arrowWidth = 7;
+
+    var nodeGroup = p.append("g")
+      .attr("class", "nodeGroup");
+
+    var node = nodeGroup.selectAll("g.node")
+      .data(path.nodes)
+      .enter()
+      .append("g")
+      .attr("class", "node");
+    node.append("rect")
+      .attr("x", function (d, i) {
+        return (i * nodeWidth) + (i * edgeSize);
+      })
+      .attr("y", posY + vSpacing)
+      .attr("width", nodeWidth)
+      .attr("height", nodeHeight);
+    //.attr("fill", "rgb(200,200,200)")
+    //.attr("stroke", "rgb(30,30,30)");
+
+    node.append("text")
+      .text(function (d) {
+        var text = d.properties["name"];
+        if (text.length > 7) {
+          text = text.substring(0, 7);
+        }
+        return text;
+      })
+      .attr("x", function (d, i) {
+        return (i * nodeWidth) + (i * edgeSize) + nodeWidth / 2;
+      })
+      .attr("y", posY + vSpacing + nodeHeight - 5);
+
+    var edgeGroup = p.append("g")
+      .attr("class", "edgeGroup");
+
+    var edge = edgeGroup.selectAll("g.edge")
+      .data(path.edges)
+      .enter()
+      .append("g")
+      .attr("class", "edge");
+
+    edge.append("line")
+      .attr("x1", function (d, i) {
+        if (isSourceNodeLeft(path.nodes, d, i)) {
+          return ((i + 1) * nodeWidth) + (i * edgeSize);
+        } else {
+          return ((i + 1) * nodeWidth) + ((i + 1) * edgeSize);
+        }
+      })
+      .attr("y1", posY + vSpacing + nodeHeight / 2)
+      .attr("x2", function (d, i) {
+        if (isSourceNodeLeft(path.nodes, d, i)) {
+          return ((i + 1) * nodeWidth) + ((i + 1) * edgeSize) - arrowWidth;
+        } else {
+          return ((i + 1) * nodeWidth) + (i * edgeSize) + arrowWidth;
+        }
+      })
+      .attr("y2", posY + vSpacing + nodeHeight / 2)
+      .attr("marker-end", "url(#arrowRight)");
+    //.attr("text-anchor", "middle")
+    //.attr("font-family", "sans-serif")
+    //.attr("font-size", "11px")
+    //.attr("fill", "rgb(30,30,30)");
+  }
+
+  function isSourceNodeLeft(nodes, edge, edgeIndex) {
+    return nodes[edgeIndex].id === edge.sourceNodeId;
+  }
 
 
 })
