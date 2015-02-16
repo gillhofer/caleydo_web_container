1a90cefe6eb289a689948bab73dbc1e02450637a
Christian Partl <partl@icg.tugraz.at>
2015-02-16 12:22:41 +0100

    simple set vis for paths

--- plugins/caleydo-vis-pathway/app.js
+++ plugins/caleydo-vis-pathway/app.js
@@ -100,106 +100,106 @@ require(['jquery', 'd3', '../caleydo/main', '../caleydo/data', '../caleydo/plugi
 
         //console.time("time")
         //
-        $.ajax({
-
-          // The 'type' property sets the HTTP method.
-          // A value of 'PUT' or 'DELETE' will trigger a preflight request.
-          //type: 'POST',
-          type: 'GET',
-
-          // The URL to make the request to.
-          //url: 'http://localhost:7474/db/data/ext/KShortestPaths/graphdb/kShortestPaths',
-          url: '/api/pathway/path',
-
-          accepts: 'application/json',
-
-          // The 'contentType' property sets the 'Content-Type' header.
-          // The JQuery default for this property is
-          // 'application/x-www-form-urlencoded; charset=UTF-8', which does not trigger
-          // a preflight. If you set this value to anything other than
-          // application/x-www-form-urlencoded, multipart/form-data, or text/plain,
-          // you will trigger a preflight request.
-          contentType: 'application/json',
-
-          //data: '{"source":"http://localhost/:7474/db/data/node/15991", ' +
-          //'"target":"http://localhost/:7474/db/data/node/1713", ' +
-          //'"k":"3", ' +
-          //'"costFunction":"function getCost(properties) {return 1.0}", ' +
-          //'"ignoreDirection":"false"}',
-          //data: '{"node_from": "/node/116", "node_to": "/node/92", "relationship_types": ["PATHWAY_EDGE"], "relationship_costs": [1.0], "only_one_route": true, "soft_timeout": 5000, "max_cost": 1000000.0 }',
-
-          //xhrFields: {
-          // The 'xhrFields' property sets additional fields on the XMLHttpRequest.
-          // This can be used to set the 'withCredentials' property.
-          // Set the value to 'true' if you'd like to pass cookies to the server.
-          // If this is enabled, your server must respond with the header
-          // 'Access-Control-Allow-Credentials: true'.
-          //withCredentials: false
-          //},
-
-          //headers: {
-          // Set any custom headers here.
-          // If you set any non-simple headers, your server must include these
-          // headers in the 'Access-Control-Allow-Headers' response header.
-          //},
-
-          success: function (response) {
-
-            console.timeEnd("time")
-            var paths = JSON.parse(response);
-
-            //var strp = paths.toString();
-
-            loadPaths(paths);
-
-            //if (paths.length > 0) {
-            //
-            //  renderPaths(svg, paths);
-            //
-            //  var firstPath = paths[0];
-            //  var firstNode = firstPath.nodes[0];
-            //  var lastNode = firstPath.nodes[firstPath.nodes.length - 1];
-            //
-            //  var pathGraph = getGraphFromPaths(paths);
-            //
-            //  renderGraph(svg2, pathGraph, firstNode, lastNode);
-            //}
-          },
-
-          error: function (response) {
-            // Here's where you handle an error response.
-            // Note that if the error was due to a CORS issue,
-            // this function will still fire, but there won't be any additional
-            // information about the error.
-            var x = 2
-          }
-        });
-
-        $.ajax({
-          type: 'GET',
-          url: '/api/pathway/summary',
-          accepts: 'application/json',
-
-          success: function (response) {
-
-            var summaryData = JSON.parse(response);
-
-            d3.selectAll("body p")
-              .data(summaryData)
-              .enter()
-              .append("p")
-              .text(function (d) {
-                return d[0].toString() + ": " + d[1].toString();
-              });
-          },
-
-          error: function (response) {
-          }
-        });
-
-        //$.getJSON("testpaths1.json", function (paths) {
-        //  loadPaths(paths);
+        //$.ajax({
+        //
+        //  // The 'type' property sets the HTTP method.
+        //  // A value of 'PUT' or 'DELETE' will trigger a preflight request.
+        //  //type: 'POST',
+        //  type: 'GET',
+        //
+        //  // The URL to make the request to.
+        //  //url: 'http://localhost:7474/db/data/ext/KShortestPaths/graphdb/kShortestPaths',
+        //  url: '/api/pathway/path',
+        //
+        //  accepts: 'application/json',
+        //
+        //  // The 'contentType' property sets the 'Content-Type' header.
+        //  // The JQuery default for this property is
+        //  // 'application/x-www-form-urlencoded; charset=UTF-8', which does not trigger
+        //  // a preflight. If you set this value to anything other than
+        //  // application/x-www-form-urlencoded, multipart/form-data, or text/plain,
+        //  // you will trigger a preflight request.
+        //  contentType: 'application/json',
+        //
+        //  //data: '{"source":"http://localhost/:7474/db/data/node/15991", ' +
+        //  //'"target":"http://localhost/:7474/db/data/node/1713", ' +
+        //  //'"k":"3", ' +
+        //  //'"costFunction":"function getCost(properties) {return 1.0}", ' +
+        //  //'"ignoreDirection":"false"}',
+        //  //data: '{"node_from": "/node/116", "node_to": "/node/92", "relationship_types": ["PATHWAY_EDGE"], "relationship_costs": [1.0], "only_one_route": true, "soft_timeout": 5000, "max_cost": 1000000.0 }',
+        //
+        //  //xhrFields: {
+        //  // The 'xhrFields' property sets additional fields on the XMLHttpRequest.
+        //  // This can be used to set the 'withCredentials' property.
+        //  // Set the value to 'true' if you'd like to pass cookies to the server.
+        //  // If this is enabled, your server must respond with the header
+        //  // 'Access-Control-Allow-Credentials: true'.
+        //  //withCredentials: false
+        //  //},
+        //
+        //  //headers: {
+        //  // Set any custom headers here.
+        //  // If you set any non-simple headers, your server must include these
+        //  // headers in the 'Access-Control-Allow-Headers' response header.
+        //  //},
+        //
+        //  success: function (response) {
+        //
+        //    console.timeEnd("time")
+        //    var paths = JSON.parse(response);
+        //
+        //    //var strp = paths.toString();
+        //
+        //    loadPaths(paths);
+        //
+        //    //if (paths.length > 0) {
+        //    //
+        //    //  renderPaths(svg, paths);
+        //    //
+        //    //  var firstPath = paths[0];
+        //    //  var firstNode = firstPath.nodes[0];
+        //    //  var lastNode = firstPath.nodes[firstPath.nodes.length - 1];
+        //    //
+        //    //  var pathGraph = getGraphFromPaths(paths);
+        //    //
+        //    //  renderGraph(svg2, pathGraph, firstNode, lastNode);
+        //    //}
+        //  },
+        //
+        //  error: function (response) {
+        //    // Here's where you handle an error response.
+        //    // Note that if the error was due to a CORS issue,
+        //    // this function will still fire, but there won't be any additional
+        //    // information about the error.
+        //    var x = 2
+        //  }
         //});
+
+        //$.ajax({
+        //  type: 'GET',
+        //  url: '/api/pathway/summary',
+        //  accepts: 'application/json',
+        //
+        //  success: function (response) {
+        //
+        //    var summaryData = JSON.parse(response);
+        //
+        //    d3.selectAll("body p")
+        //      .data(summaryData)
+        //      .enter()
+        //      .append("p")
+        //      .text(function (d) {
+        //        return d[0].toString() + ": " + d[1].toString();
+        //      });
+        //  },
+        //
+        //  error: function (response) {
+        //  }
+        //});
+
+        $.getJSON("testpaths1.json", function (paths) {
+          loadPaths(paths);
+        });
 
         function loadPaths(paths) {
 
@@ -326,8 +326,8 @@ require(['jquery', 'd3', '../caleydo/main', '../caleydo/data', '../caleydo/plugi
 
         graph.nodes.forEach(function (node) {
 
-          if(node.fixed) {
-            node.x ++;
+          if (node.fixed) {
+            node.x++;
           }
 
 
@@ -578,7 +578,6 @@ require(['jquery', 'd3', '../caleydo/main', '../caleydo/data', '../caleydo/plugi
 
     function renderPaths(svg, paths) {
 
-      svg.attr("height", 50 * paths.length);
 
       var posX = 20;
       var posY = 20;
@@ -592,9 +591,34 @@ require(['jquery', 'd3', '../caleydo/main', '../caleydo/data', '../caleydo/plugi
           .on("click", function () {
             pathListeners.notify(path);
           });
+        addPathSets(path);
         renderPath(p, path, posX, posY);
-        posY += 50;
+        posY += 50 + path.sets.length * 10;
       });
+
+      svg.attr("height", posY);
+    }
+
+    function addPathSets(path) {
+      var setDict = {};
+      var setList = [];
+
+      for (var i = 0; i < path.edges.length; i++) {
+        var edge = path.edges[i];
+        edge.properties["pathways"].forEach(function (pathwayId) {
+
+          var mySet = setDict[pathwayId];
+          if (typeof mySet == "undefined") {
+            mySet = {id: pathwayId, relIndices: [i]};
+            setDict[pathwayId] = mySet;
+            setList.push(mySet);
+          } else {
+            mySet.relIndices.push(i);
+          }
+        });
+      }
+
+      path.sets = setList;
     }
 
     function renderPath(p, path, posX, posY) {
@@ -662,6 +686,56 @@ require(['jquery', 'd3', '../caleydo/main', '../caleydo/data', '../caleydo/plugi
         })
         .attr("y2", posY + vSpacing + nodeHeight / 2)
         .attr("marker-end", "url(#arrowRight)");
+
+      var setGroup = p.append("g")
+        .attr("class", "setGroup");
+
+      var set = setGroup.selectAll("g.set")
+        .data(path.sets)
+        .enter()
+        .append("g")
+        .attr("class", "set");
+
+      set.append("text")
+        .text(function (d) {
+          var text = d.id;
+          if (text.length > 10) {
+            text = text.substring(0, 10);
+          }
+          return text;
+        })
+        .attr("x", 0)
+        .attr("y", function (d, i) {
+          return posY + 2 * vSpacing + nodeHeight + i * 10;
+        });
+
+      set.selectAll("line")
+        .data(function (d, i) {
+          return d.relIndices.map(function (item) {
+            return [item, i];
+          });
+        })
+        .enter()
+        .append("line")
+        .attr("x1", function (d) {
+          return (d[0] * nodeWidth) + (d[0] * edgeSize) + nodeWidth / 2;
+
+        })
+        .attr("y1", function (d) {
+          return posY + 2 * vSpacing + nodeHeight + d[1] * 10 - 3;
+        })
+        .attr("x2", function (d, i) {
+          return ((d[0] + 1) * nodeWidth) + ((d[0] + 1) * edgeSize) + nodeWidth / 2;
+        })
+        .attr("y2", function (d) {
+          return posY + 2 * vSpacing + nodeHeight + d[1] * 10 - 3;
+        });
+
+      //path.sets.forEach(function (s) {
+      //
+      //});
+
+
       //.attr("text-anchor", "middle")
       //.attr("font-family", "sans-serif")
       //.attr("font-size", "11px")
