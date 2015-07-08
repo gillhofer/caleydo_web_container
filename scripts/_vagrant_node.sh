7aaedf006053c14e209be6cfcdc95d796556ec81
Samuel Gratzl <samuel_gratzl@gmx.at>
2015-07-08 14:10:57 +0200

    install node packages during provisioning

--- scripts/_vagrant_node.sh
+++ scripts/_vagrant_node.sh
@@ -10,3 +10,18 @@ sudo apt-get install -y nodejs
 
 #install some global packages
 sudo npm install -g grunt-cli bower
+
+#install the node packages
+bak="`pwd`"
+cd /vagrant
+
+#create a symlink version of the node modueles for better performance
+if ![ -d "node_modules" ] ; then
+  ln -s ~/vagrant_node_modules ./node_modules
+fi
+
+set -vx #to turn echoing on and
+npm install
+set +vx
+
+cd $bak
