fb07590ac3cb4ff23df161ccd6ef816943a7d1d0
Samuel Gratzl <samuel_gratzl@gmx.at>
2015-08-11 16:45:14 +0200

    check for already installed packages

--- scripts/_vagrant_neo4j.sh
+++ scripts/_vagrant_neo4j.sh
@@ -5,18 +5,22 @@ echo "--- Start neo4j provision ---"
 # neo4j
 # following debian.neo4j.org
 
-#neo4j add to packages
-wget -O - http://debian.neo4j.org/neotechnology.gpg.key| sudo apt-key add - # Import our signing key
-echo 'deb http://debian.neo4j.org/repo stable/' | sudo tee /etc/apt/sources.list.d/neo4j.list # Create an Apt sources.list file
+if [ $(dpkg-query -W -f='${Status}' neo4j 2>/dev/null | grep -c "ok installed") -eq 0 ]; then
+  #neo4j add to packages
+  wget -O - http://debian.neo4j.org/neotechnology.gpg.key| sudo apt-key add - # Import our signing key
+  echo 'deb http://debian.neo4j.org/repo stable/' | sudo tee /etc/apt/sources.list.d/neo4j.list # Create an Apt sources.list file
 
-sudo apt-get update
-sudo apt-get install -y neo4j
+  sudo apt-get update
+  sudo apt-get install -y neo4j
 
-#TODO change the neo4j-server.properties e.g using grep or sed
-#listens to all connections even from outside by uncomenting a config line
-sudo sed -i '/^#.*0.0.0.0/s/^#//' /etc/neo4j/neo4j-server.properties
-#disable authorization by default
-sudo sed -i '/dbms.security.auth_enabled=true/s/dbms.security.auth_enabled=false/' /etc/neo4j/neo4j-server.properties
-#stop server:
-sudo service neo4j-service stop
-#access the browser interface via: http://localhost:7474/
+  #TODO change the neo4j-server.properties e.g using grep or sed
+  #listens to all connections even from outside by uncomenting a config line
+  sudo sed -i '/^#.*0.0.0.0/s/^#//' /etc/neo4j/neo4j-server.properties
+  #disable authorization by default
+  sudo sed -i '/dbms.security.auth_enabled=true/s/dbms.security.auth_enabled=false/' /etc/neo4j/neo4j-server.properties
+  #stop server:
+  sudo service neo4j-service stop
+  #access the browser interface via: http://localhost:7474/
+else
+  echo "neo4j already installed"
+fi
--- scripts/_vagrant_node.sh
+++ scripts/_vagrant_node.sh
@@ -4,12 +4,16 @@ echo "--- Start node provisioning ---"
 #########################
 #node-js
 
-sudo apt-get install -y curl
-curl -sL https://deb.nodesource.com/setup | sudo bash -
-sudo apt-get install -y nodejs
+if [ $(dpkg-query -W -f='${Status}' nodejs 2>/dev/null | grep -c "ok installed") -eq 0 ]; then
+  sudo apt-get install -y curl
+  curl -sL https://deb.nodesource.com/setup | sudo bash -
+  sudo apt-get install -y nodejs
 
-#install some global packages
-sudo npm install -g grunt-cli bower tsd
+  #install some global packages
+  sudo npm install -g grunt-cli bower tsd
+else
+  echo "node already installed"
+fi
 
 #install the node packages
 bak="`pwd`"
@@ -22,10 +26,9 @@ if [[ ! -d "node_modules" ]]; then
   ln -s /home/vagrant/caleydo_web/node_modules/ ./node_modules
 fi
 
-
 echo "--- installing node dependencies ---"
 set -vx #to turn echoing on and
 sudo npm install
 set +vx
 
-cd $bak
+cd ${bak}
